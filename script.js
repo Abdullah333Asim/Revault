e4f51e4944ca086393d69132642769de4e51b518
Abdullah333Asim <abdullah333asim@gmail.com>
2025-07-04 16:21:28 +0500

    added fadeup animations

--- script.js
+++ script.js
@@ -140,3 +140,14 @@ function handleSwipe() {
     showSlide(currentSlide);
   }
 }
+
+// fade-in animation
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+      }
+    });
+  });
+
+  document.querySelectorAll('.fade-in').forEach((el) => observer.observe(el));
